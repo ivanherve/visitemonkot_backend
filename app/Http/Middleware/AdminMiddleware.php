40fbc5baebeeb108d68f39da9f92f8f73400e0f4
ivanherve <ivanherve1411@gmail.com>
2019-06-19 17:50:52 +0200

    Admin interface + new classes

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Token;
+use App\User;
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = Token::all()->where('api_token','=',$request->header('api_token'))->first();
+        $user = User::all()->where('user_id','=',$token->user_id)->first();
+        if($user->profil_id != 3){
+            return response(['status'=>'error','response' => ['Unauthorized.']], 401);
+        }
+        return $next($request);
+    }
+}
