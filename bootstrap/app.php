6ef6c4b97244442836798dc09ed8da684dde6076
ivanherve <ivanherve1411@gmail.com>
2019-07-15 11:46:25 +0200

    Mailing Libraries

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -87,9 +87,9 @@ $app->register(Illuminate\Mail\MailServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 $app->configure('mail');
-$app->alias('mailer', Illuminate\Mail\Mailer::class);
-$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
-$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
+//$app->alias('mailer', Illuminate\Mail\Mailer::class);
+//$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+//$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
 /*
 |--------------------------------------------------------------------------
